41de9902961ab94b4aadab29d43090423f5c8781
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-02-08 16:27:31 +0100

    modification affichage requete get title et select

--- backend/src/models/SubjectManager.js
+++ backend/src/models/SubjectManager.js
@@ -7,7 +7,7 @@ class SubjectManager extends AbstractManager {
 
   getAll() {
     return this.database
-      .query(`SELECT subject.id, subject.text, subject.title, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
+      .query(`SELECT subject.id, subject.title, subject.text, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
       FROM devs_corner.subject
       INNER JOIN devs_corner.subject_has_tag ON subject.id = subject_has_tag.subject_id
       INNER JOIN devs_corner.tag ON subject_has_tag.tag_id = tag.id
@@ -18,7 +18,7 @@ class SubjectManager extends AbstractManager {
 
   getAllOneQuery(tagId) {
     return this.database.query(
-      `SELECT subject.id, subject.text, subject.title, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
+      `SELECT subject.id, subject.title, subject.text, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
         FROM devs_corner.subject
         INNER JOIN devs_corner.subject_has_tag ON subject.id = subject_has_tag.subject_id
         INNER JOIN devs_corner.tag ON subject_has_tag.tag_id = tag.id
@@ -32,7 +32,7 @@ class SubjectManager extends AbstractManager {
   getAllQuery(tagId) {
     const placeholders = tagId.map(() => `?`).join(",");
     return this.database.query(
-      `SELECT subject.id, subject.text, subject.title, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
+      `SELECT subject.id, subject.title, subject.text, subject.status_resolve, subject.created_at, JSON_ARRAYAGG(tag.name) as tags, concat(user.first_name, " ", user.last_name ) as Auteur
         FROM devs_corner.subject
         INNER JOIN devs_corner.subject_has_tag ON subject.id = subject_has_tag.subject_id
         INNER JOIN devs_corner.tag ON subject_has_tag.tag_id = tag.id
