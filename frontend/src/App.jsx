6895ad3e848c687d68a29aa68227c0b16a85b550
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-02-15 17:46:56 +0100

    ajout context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,20 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable import/no-extraneous-dependencies */
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-
-import Landing from "./pages/landing/Landing";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import SubjectList from "./pages/subjectsList/SubjectsList";
-import Subject from "./pages/subjet/Subject";
-import Account from "./pages/account/Account";
+import { useState } from "react";
+import PublicRoutes from "./services/routes/PublicRoutes";
+import PrivateRoute from "./services/routes/PrivateRoute";
+import TokenContext from "./services/contexts/TokenContext";
 
 function App() {
-  const isLoggedIn = localStorage.getItem("token");
-
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    sessionStorage.getItem("token") !== null
+  );
+  // eslint-disable-next-line react/jsx-no-useless-fragment
   return (
-    <Router>
-      <Routes>
-        {isLoggedIn ? (
-          <>
-            <Route path="/" element={<SubjectList />} />
-            <Route path="/subject/:id" element={<Subject />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="*" element={<Navigate replace to="/" />} />
-          </>
-        ) : (
-          <>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate replace to="/" />} />
-          </>
-        )}
-      </Routes>
-    </Router>
+    <TokenContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {isLoggedIn ? <PrivateRoute /> : <PublicRoutes />}
+    </TokenContext.Provider>
   );
 }
 
--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
+import TokenContext from "../../services/contexts/TokenContext";
+
 export default function Landing() {
-  const handleclick = () => {
-    localStorage.setItem(
+  const { setIsLoggedIn } = useContext(TokenContext);
+  const navigate = useNavigate();
+
+  const handleclick = async () => {
+    await sessionStorage.setItem(
       "token",
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjcsInVzZXJSb2xlIjoidXNlciIsInVzZXJTdGF0ZSI6MSwiaWF0IjoxNjc2NDE0MTIxLCJleHAiOjE2NzY1MDA1MjF9._JiRl-zBINxA7Nsfjqd0Xgnf6H3LsWdMntLqhvO05G0"
     );
+    setIsLoggedIn(true);
+    navigate("/");
   };
   return (
     <div>
