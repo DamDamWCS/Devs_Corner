88973524633bfe6935cf18190244f62b13270fd7
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-02-17 01:17:11 +0100

    visuel header/footer + navigation login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -6,7 +6,6 @@ function Login() {
   const { setIsLoggedIn } = useContext(TokenContext);
   const navigate = useNavigate();
   const handleclick = async () => {
-
     await sessionStorage.setItem(
       "token",
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjcsInVzZXJSb2xlIjoidXNlciIsInVzZXJTdGF0ZSI6MSwiaWF0IjoxNjc2NDE0MTIxLCJleHAiOjE2NzY1MDA1MjF9._JiRl-zBINxA7Nsfjqd0Xgnf6H3LsWdMntLqhvO05G0"
