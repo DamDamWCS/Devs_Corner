4b231b13313be667a3042233cf371196d8abddb2
DamDamWCS <bobon.damien.wcs@gmail.com>
2023-02-20 12:04:54 +0100

    modification padding pages

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -4,7 +4,7 @@ import "./Landing.css";
 export default function Landing() {
   return (
     <>
-      <div className="b-shadow flex-grow-1 m-4 rounded bg-white text-center">
+      <div className="b-shadow flex-grow-1 rounded bg-white text-center m-3 m-sm-4">
         <h1>PAGE LANDING</h1>
         <p>Test ICON :</p>
         <i className="icons-checked icons-size-30px" aria-hidden="true" />
